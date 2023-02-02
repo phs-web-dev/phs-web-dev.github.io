22dbdf0ec22e146815ddb4017775a4d1172e635a
Stephen <2stph16@gmail.com>
2023-02-02 18:34:07 -0500

    Fixed offline

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,17 @@
-const CACHE_NAME = 'phs-web-dev';
 
-self.addEventListener('fetch', event => {
-    const isOnline = navigator.onLine;
-    if (!isOnline) {
-        // User is offline, serve the cached version of the resource
-        event.respondWith(caches.match(event.request));
-    } else {
-        // User is online, fetch the latest version of the resource from the server
-        event.respondWith(
-            fetch(event.request).then(response => {
-                // Once the response is received, cache it for future offline use
-                caches.open(CACHE_NAME).then(cache => {
-                    cache.put(event.request, response.clone());
-                });
-                return response;
+const CACHE_NAME = `phs-web-dev`;
 
-            })
-        );
-    }
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        (async function () {
+            const cache = await caches.open(CACHE_NAME);
+            try {
+                const networkResponse = await fetch(event.request);
+                event.waitUntil(cache.put(event.request, networkResponse.clone()));
+                return networkResponse;
+            } catch (err) {
+                return cache.match(event.request);
+            }
+        })(),
+    );
 });
