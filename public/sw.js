3cacd42b70ce53c34fd422854d4d938fdbcc6a6e
Stephen <2stph16@gmail.com>
2023-02-02 17:51:00 -0500

    Only use cache if offline

--- public/sw.js
+++ public/sw.js
@@ -1,38 +1,21 @@
 const CACHE_NAME = 'phs-web-dev';
 
-// A list of resources to cache
-const RESOURCES_TO_PRELOAD = [
-    '/',
-    '/schedule',
-];
-
-// The install event listener, which caches all the resources
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(RESOURCES_TO_PRELOAD);
-            })
-    );
-});
-
-// The fetch event listener, which serves resources from the cache if possible,
-// and otherwise makes a network request and adds the response to the cache
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request)
-                    .then(networkResponse => {
-                        caches.open(CACHE_NAME)
-                            .then(cache => {
-                                cache.put(event.request, networkResponse.clone());
-                            });
-                        return networkResponse;
-                    });
+    const isOnline = navigator.onLine;
+    if (!isOnline) {
+        // User is offline, serve the cached version of the resource
+        event.respondWith(caches.match(event.request));
+    } else {
+        // User is online, fetch the latest version of the resource from the server
+        event.respondWith(
+            fetch(event.request).then(response => {
+                // Once the response is received, cache it for future offline use
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, response.clone());
+                });
+                return response;
+
             })
-    );
+        );
+    }
 });
