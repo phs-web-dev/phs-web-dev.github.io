91cdd2df6f6b2ce2a9f5981b94d1052a021ee770
Stephen <2stph16@gmail.com>
2023-02-02 23:38:12 -0500

    Settings modal

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://phs-web-dev.github.io',
-  integrations: [tailwind()]
+  site: "https://phs-web-dev.github.io",
+  integrations: [tailwind()],
 });
--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,17 @@
-
 const CACHE_NAME = `phs-web-dev`;
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        (async function () {
-            const cache = await caches.open(CACHE_NAME);
-            try {
-                const networkResponse = await fetch(event.request);
-                event.waitUntil(cache.put(event.request, networkResponse.clone()));
-                return networkResponse;
-            } catch (err) {
-                return cache.match(event.request);
-            }
-        })(),
-    );
+self.addEventListener("fetch", (event) => {
+  if (!(event.request.url.indexOf("http") === 0)) return;
+  event.respondWith(
+    (async function () {
+      const cache = await caches.open(CACHE_NAME);
+      try {
+        const networkResponse = await fetch(event.request);
+        event.waitUntil(cache.put(event.request, networkResponse.clone()));
+        return networkResponse;
+      } catch (err) {
+        return cache.match(event.request);
+      }
+    })()
+  );
 });
