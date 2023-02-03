eefbfaea9615d1dbcc0e8a406b92a8f124ddb35d
Stephen <2stph16@gmail.com>
2023-02-03 13:37:15 -0500

    Fixed confetti

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,20 @@
 /// <reference types="astro/client" />
+
+import type JSConfetti from "js-confetti";
+
+declare global {
+  interface Window {
+    jsConfetti: JSConfetti;
+  }
+
+  interface Schedule {
+    type: string;
+    periods: Period[];
+  }
+
+  interface Period {
+    period: string;
+    startTime: number;
+    endTime: number;
+  }
+}
